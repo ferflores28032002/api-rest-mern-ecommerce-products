d4927c73ae0dae60a3cc4420518e104adf45722b
fernando flores <martinjanns2000@gmail.com>
2022-12-21 12:45:15 -0600

    modificado

--- routes/shoes.js
+++ routes/shoes.js
@@ -4,10 +4,10 @@ import { addImagenes, addImagenProducts, loadProductos, loadProductsForId } from
 
 const router = Router();
 
-router.get("/products/shoes", loadProductos);
-router.post("/products/shoes", addImagenes);
-router.post("/products/shoes/:id", addImagenProducts);
-router.get("/products/shoes/:id", loadProductsForId);
+router.get("/shoes", loadProductos);
+router.post("/shoes", addImagenes);
+router.post("/shoes/:id", addImagenProducts);
+router.get("/shoes/:id", loadProductsForId);
 
 
 export default router;
