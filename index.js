76f21015ec4802244ae1f6c23ee6e3164379c924
fernando flores <martinjanns2000@gmail.com>
2022-12-25 10:25:55 -0600

    se modifico cors

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const app = express();
 
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json({ limit: "50mb" }));
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 
 // RUTAS QUE UNEN A LOS CONTROLADORES Y LOS MODELOS
 app.use(productosRouter);
