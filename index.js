b67a86b19e6252fd3c768a145214a1371b702d0c
fernando flores <martinjanns2000@gmail.com>
2022-12-27 10:21:39 -0600

    se configuro cors

--- index.js
+++ index.js
@@ -10,9 +10,14 @@ import "./databases/conexion.js";
 
 const app = express();
 
+const corsOptions ={
+  origin:'https://fashion-s-ecommerce.netlify.app', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json({ limit: "50mb" }));
-app.use(cors());
+app.use(cors(corsOptions));
 
 // RUTAS QUE UNEN A LOS CONTROLADORES Y LOS MODELOS
 app.use(productosRouter);
